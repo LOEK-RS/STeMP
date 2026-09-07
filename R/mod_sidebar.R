25a7d7443b4e808e6ba02cb93504faea28d8587a
JanLinnenbrink <jan.linnenbrink@web.de>
2026-09-07 12:34:51 +0200

    reorders temporal elements and fixes progress bar

--- R/mod_sidebar.R
+++ R/mod_sidebar.R
@@ -74,13 +74,21 @@ mod_sidebar_server <- function(
 		filtered_protocol_data <- shiny::reactive({
 			shiny::req(protocol_data())
 			df <- protocol_data()
-			# Keep all rows, but optionally mark optional rows
+			dict <- protocol_dict()
+
 			df$visible <- TRUE
+
 			if (isTRUE(input$hide_optional)) {
 				df$visible[df$optional == 1] <- FALSE
 			}
-			caption_ids <- protocol_dict()$element_id[protocol_dict()$element_type == "figure_caption"]
+
+			caption_ids <- dict$element_id[dict$element_type == "figure_caption"]
 			df$visible[df$element_id %in% caption_ids] <- FALSE
+
+			# Remove "spatio-temporal" vs "spatial" toggle from the progress bar
+			switch_ids <- dict$element_id[dict$element_type == "radio"]
+			df$visible[df$element_id %in% switch_ids] <- FALSE
+
 			df
 		})
 
